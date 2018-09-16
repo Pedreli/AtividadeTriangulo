934487db2551644dbee736d2979b72b9b551b487
Felipe Pedreli Pereira <32180531+Pedreli@users.noreply.github.com>
2018-09-16 16:14:42 -0300

    Implementado triangulo na classe main

--- src/atividadetriangulo/AtividadeTriangulo.java
+++ src/atividadetriangulo/AtividadeTriangulo.java
@@ -20,36 +20,10 @@ public class AtividadeTriangulo {
         System.out.println("Informa o valor do lado c");
         double c = sc.nextDouble();
 
-        Set<String> propriedades = new HashSet<String>();
-        double hipotenusa = a;
-        if (hipotenusa < b) {
-            hipotenusa = b;
-        }
-        if (hipotenusa < c) {
-            hipotenusa = c;
-        }
-        if ((a + b + c - hipotenusa) > hipotenusa) {
-            propriedades.add("Válido");
-            if (a == b || b == c || c == a) {
-                if (a == b && b == c) {
-                    propriedades.add("Equilátero");
-                } else {
-                    propriedades.add("Isósceles");
-                }
-            }
-            if (a != b && b != c && c != a) {
-                propriedades.add("Escaleno");
-            }
-            if (((a * a + b * b + c * c) - hipotenusa * hipotenusa) == hipotenusa
-                    * hipotenusa) {
-                propriedades.add("Retângulo");
-            }
-        } else {
-            propriedades.add("Inválido");
-        }
-
+        Triangulo triangulo = new Triangulo(a, b, c);
+        
         System.out.println("Propriedades do triangulo ->");
-        for (String valor : propriedades) {
+        for (String valor : triangulo.getPropriedades()) {
             System.out.println(valor);
 
         }
